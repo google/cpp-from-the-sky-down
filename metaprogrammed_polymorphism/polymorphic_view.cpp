edd625ebcbda01cf70dc99484c0916c7e05e1eb5
John Bandela <jbandela@gmail.com>
2018-08-15 20:15:09 -0700

    Add ability to subset polymorphic view

--- metaprogrammed_polymorphism/polymorphic_view.cpp
+++ metaprogrammed_polymorphism/polymorphic_view.cpp
@@ -9,13 +9,18 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 
 #include <array>
 #include <utility>
 
 namespace polymorphic {
 namespace detail {
+
+template <typename T>
+using ptr = T*;
 template <typename T, typename Signature>
 struct function_entry;
 
@@ -28,24 +33,27 @@ struct function_entry<T, Return(Method, Parameters...)> {
 
 template <typename T, typename... Signatures>
 inline const auto vtable = std::array{
-    reinterpret_cast<void (*)()>(function_entry<T, Signatures>::poly_call)...};
+    reinterpret_cast<ptr<void()>>(function_entry<T, Signatures>::poly_call)...};
 
 template <size_t Index, typename Signature>
 struct vtable_caller;
 template <size_t Index, typename Return, typename Method,
           typename... Parameters>
 struct vtable_caller<Index, Return(Method, Parameters...)> {
-  template <size_t Size>
-  Return call_vtable(Method method, const std::array<void (*)(), Size>& table,
-                     void* t, Parameters... parameters) {
-    return reinterpret_cast<Return (*)(Method, void*, Parameters...)>(
+  static Return call_vtable(Method method, ptr<const ptr<void()>> table,
+                            void* t, Parameters... parameters) {
+    return reinterpret_cast<ptr<Return(Method, void*, Parameters...)>>(
         table[Index])(method, t, parameters...);
+  }
+  constexpr static std::size_t get_index(ptr<Return(Method, Parameters...)>) {
+    return Index;
   }
 };
 
 template <typename... implementations>
 struct overload_call_vtable : implementations... {
   using implementations::call_vtable...;
+  using implementations::get_index...;
 };
 
 template <typename IntPack, typename... Signatures>
@@ -56,24 +64,55 @@ struct polymorphic_caller_implementation<std::index_sequence<I...>,
     : overload_call_vtable<vtable_caller<I, Signatures>...> {};
 
 }  // namespace detail
+template <typename... Signatures>
+class polymorphic_view;
+
+template <typename T>
+struct is_polymorphic_view {
+  static constexpr bool value = false;
+};
+
+template <typename... Signatures>
+struct is_polymorphic_view<polymorphic_view<Signatures...>> {
+  static constexpr bool value = true;
+};
 
 template <typename... Signatures>
 class polymorphic_view
     : private detail::polymorphic_caller_implementation<
           std::make_index_sequence<sizeof...(Signatures)>, Signatures...> {
-  const std::array<void (*)(), sizeof...(Signatures)>* vptr_ = nullptr;
+  const std::array<detail::ptr<void()>, sizeof...(Signatures)>* vptr_ = nullptr;
   void* t_ = nullptr;
 
+  template <typename... OtherSignatures>
+  static auto get_vtable(const polymorphic_view<OtherSignatures...>& other) {
+    static const std::array<detail::ptr<void()>, sizeof...(Signatures)> vtable{
+        other.get_entry<Signatures>()...};
+    return &vtable;
+  }
+  template <typename Signature>
+  detail::ptr<void()> get_entry() const {
+    return (*vptr_)[this->get_index(detail::ptr<Signature>{})];
+  }
+
+  template <typename... OtherSignatures>
+  friend class polymorphic_view;
+
  public:
-  template <typename T>
+  template <typename T, typename = std::enable_if_t<
+                            !is_polymorphic_view<std::decay_t<T>>::value>>
   explicit polymorphic_view(T& t)
       : vptr_(&detail::vtable<std::decay_t<T>, Signatures...>), t_(&t) {}
 
-  explicit operator bool() const { return t != nullptr; }
+  template <typename... OtherSignatures>
+  explicit polymorphic_view(const polymorphic_view<OtherSignatures...>& other)
+      : vptr_(get_vtable(other)), t_(other.t_) {}
+
+  explicit operator bool() const { return t_ != nullptr; }
 
   template <typename Method, typename... Parameters>
-  decltype(auto) call(Parameters&&...) {
-    return this->call_vtable(Method{}, *vptr_, t_,
+  decltype(auto) call(Parameters&&... parameters) {
+    return this->call_vtable(Method{}, vptr_->data(), t_,
                              std::forward<Parameters>(parameters)...);
   }
 };
@@ -106,4 +145,8 @@ int main() {
   p2 = poly{s};
   p2.call<another>();
   p2.call<draw>();
+
+  polymorphic::polymorphic_view<void(draw)> p3{p2};
+
+  p3.call<draw>();
 }
