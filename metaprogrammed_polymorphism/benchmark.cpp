8f0ca7066a7e77b9f0d04576aa3d653f4758c8f7
John Bandela <jbandela@gmail.com>
2019-09-14 04:23:33 -0700

    Update polymorphism benchmark

--- metaprogrammed_polymorphism/benchmark.cpp
+++ metaprogrammed_polymorphism/benchmark.cpp
@@ -1,11 +1,11 @@
 #include "polymorphic.hpp"
 #include <benchmark/benchmark.h>
 
+#ifdef _MSC_VER
 #pragma comment(lib,"shlwapi.lib")
-
+#endif
 
 #include "benchmark_imp.h"
-
 
 
 void SomeFunction() {}
@@ -13,50 +13,59 @@ void SomeFunction() {}
 static void BM_Virtual(benchmark::State& state) {
 	auto b = MakeBase();
 	auto pb = b.get();
-  // Perform setup here
-  for (auto _ : state) {
-    // This code gets timed
-	  benchmark::DoNotOptimize(pb);
-	  benchmark::DoNotOptimize(pb->draw());
-  }
+	// Perform setup here
+	for (auto _ : state) {
+		// This code gets timed
+		benchmark::DoNotOptimize(pb->draw());
+	}
 }
 
-static void BM_Poly(benchmark::State& state) {
+static void BM_PolyRef(benchmark::State& state) {
 	Dummy d;
 	polymorphic::ref<int(draw)> ref(d);
-  // Perform setup here
-  for (auto _ : state) {
-    // This code gets timed
-	  benchmark::DoNotOptimize(ref);
-	  benchmark::DoNotOptimize(ref.call<draw>());
-  }
+	// Perform setup here
+	for (auto _ : state) {
+		// This code gets timed
+		benchmark::DoNotOptimize(ref.call<draw>());
+	}
+	static_assert(sizeof(ref) == 2 * sizeof(void*));
+}
+
+static void BM_PolyObject(benchmark::State& state) {
+	Dummy d;
+	polymorphic::object<int(draw)> ref(d);
+	// Perform setup here
+	for (auto _ : state) {
+		// This code gets timed
+		benchmark::DoNotOptimize(ref.call<draw>());
+	}
+	static_assert(sizeof(ref) == 3 * sizeof(void*));
 }
 
 static void BM_Function(benchmark::State& state) {
 	auto f = GetFunction();
 	Dummy d;
-  // Perform setup here
-  for (auto _ : state) {
-    // This code gets timed
-	  benchmark::DoNotOptimize(f);
-	  benchmark::DoNotOptimize(f(d));
-  }
+	// Perform setup here
+	for (auto _ : state) {
+		// This code gets timed
+		benchmark::DoNotOptimize(f(d));
+	}
 }
 
 static void BM_NonVirtual(benchmark::State& state) {
 	NonVirtual n;
-  // Perform setup here
-  for (auto _ : state) {
-    // This code gets timed
-	  benchmark::DoNotOptimize(n);
-	  benchmark::DoNotOptimize(n.draw());
-  }
+	// Perform setup here
+	for (auto _ : state) {
+		// This code gets timed
+		benchmark::DoNotOptimize(n.draw());
+	}
 }
 // Register the function as a benchmark
-BENCHMARK(BM_Poly);
 BENCHMARK(BM_NonVirtual);
-BENCHMARK(BM_Function);
 BENCHMARK(BM_Virtual);
+BENCHMARK(BM_Function);
+BENCHMARK(BM_PolyObject);
+BENCHMARK(BM_PolyRef);
 
 // Run the benchmark
 BENCHMARK_MAIN();
--- metaprogrammed_polymorphism/polymorphic.hpp
+++ metaprogrammed_polymorphism/polymorphic.hpp
@@ -32,46 +32,6 @@ namespace polymorphic {
 			return static_cast<typename fwd_helper<T>::type>(u);
 		}
 
-		class value_holder {
-			struct holder_interface {
-				virtual std::unique_ptr<holder_interface> clone() = 0;
-				virtual ~holder_interface() {}
-				void* ptr_ = nullptr;
-			};
-			template<typename T>
-			struct holder_impl :holder_interface {
-				holder_impl(T t) :t_(std::move(t)) { ptr_ = &t_; }
-				std::unique_ptr<holder_interface> clone() override {
-					return std::make_unique<holder_impl<T>>(t_);
-				}
-				T t_;
-			};
-			std::unique_ptr<holder_interface> impl_;
-		public:
-			template<typename T>
-			value_holder(T t) :impl_(std::make_unique<holder_impl<T>>(std::move(t))) {}
-
-			value_holder(value_holder&&) = default;
-			value_holder& operator=(value_holder&&) = default;
-
-			value_holder(const value_holder& other) :impl_(other.impl_ ? other.impl_->clone() : nullptr) {}
-			value_holder& operator=(const value_holder& other) {
-				return (*this) = value_holder(other);
-			}
-			void* get_ptr() { return impl_ ? impl_->ptr_ : nullptr; }
-			const void* get_ptr()const { return impl_ ? impl_->ptr_ : nullptr; }
-		};
-
-		template<typename T>
-		struct ptr_holder {
-			T* ptr_;
-			T* get_ptr()const { return ptr_; }
-			template<typename V>
-			ptr_holder(V& v) :ptr_(&v) {}
-			ptr_holder(value_holder& v) :ptr_(v.get_ptr()) {}
-			ptr_holder(const value_holder& v) :ptr_(v.get_ptr()) {}
-		};
-
 		template <typename T> using ptr = T*;
 		template <typename T> struct type {};
 		template <typename... F> struct overload : F... { using F::operator()...; };
@@ -133,6 +93,12 @@ namespace polymorphic {
 		template <typename Holder, typename Sequence, typename... Signatures>
 		class ref_impl;
 
+		template<typename T>
+		struct is_ref_impl :std::false_type {};
+
+		template <typename Holder, typename Sequence, typename... Signatures>
+		struct is_ref_impl<ref_impl<Holder, Sequence, Signatures...>> :std::true_type {};
+
 		template <typename Holder, size_t... I, typename... Signatures>
 		class ref_impl<Holder, std::index_sequence<I...>, Signatures...>
 			: private vtable_caller<I, Signatures>... {
@@ -160,10 +126,13 @@ namespace polymorphic {
 				return first;
 			}
 
-			struct other_ref {};
+			template <typename T>
+			ref_impl(T&& t, std::false_type)
+				: vptr_(&detail::vtable<std::decay_t<T>, Signatures...>[0]),
+				t_(std::forward<T>(t)) {}
 
 			template <typename T>
-			ref_impl(T&& other, other_ref)
+			ref_impl(T&& other, std::true_type)
 				: vptr_(other.vptr_ + get_offset<std::decay_t<T>>()),
 				t_(std::forward<T>(other).t_) {
 				static_assert(get_offset<std::decay_t<T>>() != -1);
@@ -171,30 +140,7 @@ namespace polymorphic {
 
 		public:
 			template <typename T>
-			ref_impl(T&& t)
-				: vptr_(&detail::vtable<std::decay_t<T>, Signatures...>[0]),
-				t_(std::forward<T>(t)) {}
-
-
-
-			template <typename OtherHolder, typename OtherSequence,
-				typename... OtherSignatures>
-				ref_impl(
-					const ref_impl<OtherHolder, OtherSequence, OtherSignatures...>& other)
-				: ref_impl(other, other_ref{}) {}
-
-			template <typename OtherHolder, typename OtherSequence,
-				typename... OtherSignatures>
-				ref_impl(
-					ref_impl<OtherHolder, OtherSequence, OtherSignatures...>& other)
-				: ref_impl(other, other_ref{}) {}
-
-
-
-			template <typename OtherHolder, typename OtherSequence,
-				typename... OtherSignatures>
-				ref_impl(ref_impl<OtherHolder, OtherSequence, OtherSignatures...>&& other)
-				: ref_impl(std::move(other), other_ref{}) {}
+			ref_impl(T&& t) :ref_impl(std::forward<T>(t), is_ref_impl<std::decay_t<T>>{}) {}
 
 			explicit operator bool() const { return t_ != nullptr; }
 
@@ -211,9 +157,80 @@ namespace polymorphic {
 				return call_vtable(vptr_, Method{}, t_.get_ptr(),
 					std::forward<Parameters>(parameters)...);
 			}
-
 		};
 
+		struct holder_interface {
+			virtual std::unique_ptr<holder_interface> clone()const = 0;
+			virtual ~holder_interface() {}
+			void* ptr_ = nullptr;
+		};
+		template<typename T>
+		struct holder_impl :holder_interface {
+			holder_impl(T t) :t_(std::move(t)) { ptr_ = &t_; }
+			std::unique_ptr<holder_interface> clone() const override {
+				return std::make_unique<holder_impl<T>>(t_);
+			}
+			T t_;
+		};
+
+
+		class value_holder {
+			std::unique_ptr<holder_interface> impl_;
+			void* ptr_ = nullptr;
+		public:
+			template<typename T>
+			value_holder(T t) :impl_(std::make_unique<holder_impl<T>>(std::move(t))),ptr_(get_ptr_impl()) {}
+
+			value_holder(value_holder&& other)noexcept:impl_(std::move(other.impl_)), ptr_(other.ptr_) {
+				other.ptr_ = nullptr;
+			}
+			value_holder& operator=(value_holder&& other)noexcept {
+				impl_ = std::move(other.impl_);
+				ptr_ = std::move(other.ptr_);
+				other.ptr_ = nullptr;
+			}
+
+			value_holder(const value_holder& other) :impl_(other.impl_ ? other.impl_->clone() : nullptr),ptr_(get_ptr_impl()) {}
+			value_holder& operator=(const value_holder& other) {
+				return (*this) = value_holder(other);
+			}
+			void* get_ptr_impl() { return impl_ ? impl_->ptr_ : nullptr; }
+			void* get_ptr() { return ptr_; }
+			const void* get_ptr()const { return ptr_; }
+
+			auto clone_ptr()const { return impl_ ? impl_->clone() : nullptr; }
+		};
+
+		template<typename T>
+		struct ptr_holder {
+			T* ptr_;
+			T* get_ptr()const { return ptr_; }
+			template<typename V>
+			ptr_holder(V& v) :ptr_(&v) {}
+			ptr_holder(value_holder& v) :ptr_(v.get_ptr()) {}
+			ptr_holder(const value_holder& v) :ptr_(v.get_ptr()) {}
+		};
+
+		struct shared_ptr_holder {
+			std::shared_ptr<const holder_interface> impl_;
+			const void* ptr_ = nullptr;
+			const void* get_ptr()const { return ptr_; }
+			const void* get_ptr_impl()const { return impl_?impl_->ptr_:nullptr; }
+			shared_ptr_holder(const shared_ptr_holder&) = default;
+			shared_ptr_holder& operator=(const shared_ptr_holder&) = default;
+			shared_ptr_holder(shared_ptr_holder&& other)noexcept:impl_(std::move(other.impl_)), ptr_(other.ptr_) {
+				other.ptr_ = nullptr;
+			}
+			shared_ptr_holder& operator=(shared_ptr_holder&& other)noexcept {
+				impl_ = std::move(other.impl_);
+				ptr_ = std::move(other.ptr_);
+				other.ptr_ = nullptr;
+			}
+			template<typename T>
+			shared_ptr_holder(T t) :impl_(std::make_shared<const holder_impl<T>>(std::move(t))),ptr_(get_ptr_impl()) {}
+			shared_ptr_holder(const value_holder& v) :impl_(v.clone_ptr()) ,ptr_(get_ptr_impl()){}
+			auto clone_ptr()const { return impl_ ? impl_->clone() : nullptr; }
+		};
 
 
 	} // namespace detail
@@ -227,11 +244,9 @@ namespace polymorphic {
 
 	template <typename... Signatures>
 	using object = detail::ref_impl<
-		detail::value_holder,
+		std::conditional_t<
+		std::conjunction_v<detail::is_const_signature<Signatures>...>,
+		detail::shared_ptr_holder, detail::value_holder >,
 		std::make_index_sequence<sizeof...(Signatures)>, Signatures...>;
-
-
-
-
 
 } // namespace polymorphic
