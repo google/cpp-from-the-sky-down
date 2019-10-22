60589d95df01d208ae45d2986f0ad84355abbc57
John Bandela <jbandela@gmail.com>
2019-10-22 00:30:51 -0700

    Add compile time map example

--- metaprogramming_examples/metaprogramming_examples.cpp
+++ metaprogramming_examples/metaprogramming_examples.cpp
@@ -46,7 +46,48 @@ void test_overload() {
       v);
 }
 
+#include <type_traits>
+template<typename K, typename V>
+struct pair {};
+
+template<typename... Pairs>
+struct map :Pairs...{};
+
+template<typename K, typename D, typename V>
+V value_impl(pair<K, V>);
+
+template<typename K, typename D>
+D value_impl(...);
+
+
+
+template<typename M, typename K, typename D = void>
+using value = decltype(value_impl<K,D>(M{}));
+
+
+
+void test_map() {
+
+	using m1 = map<pair<int, double>, pair<char, std::string>>;
+	using m2 = map<pair<int, float>, pair<char, int>>;
+
+	static_assert(std::is_same_v < value<m1, char>, std::string>);
+	static_assert(std::is_same_v < value<m2, char>, int>);
+	static_assert(std::is_same_v < value<m2, double,char>, char>);
+
+
+
+
+}
+
+
+
+
+
+
+
 int main() {
   test_tuple();
   test_overload();
+  test_map();
 }
