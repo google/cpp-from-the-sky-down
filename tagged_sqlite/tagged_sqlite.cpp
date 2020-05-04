b8535a55b1365accbf2ccd209e111b10c12dd669
Your Name <jbandela@gmail.com>
2020-05-04 13:11:47 -0700

    Add more comments to tagged_sqlite.cpp example

--- tagged_sqlite/tagged_sqlite.cpp
+++ tagged_sqlite/tagged_sqlite.cpp
@@ -1,5 +1,16 @@
-﻿// tagged_sqlite.cpp : Defines the entry point for the application.
+﻿// Copyright 2020 Google LL`C
 //
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #include "tagged_sqlite.h"
 
@@ -12,6 +23,9 @@ CREATE TABLE customers(id INTEGER NOT NULL PRIMARY KEY, name TEXT);
     create_orders = R"(
 CREATE TABLE orders(id INTEGER NOT NULL PRIMARY KEY, item TEXT, customerid INTEGER, price REAL);
 )",
+
+    // Annotate typed parameters via {{?tag:type}}. In the final sql these will
+    // be replaced with '?'.
     insert_customer = R"(
 INSERT INTO customers(id, name) VALUES( {{?id:int}}, {{?name:string}});
 )",
@@ -19,6 +33,10 @@ INSERT INTO customers(id, name) VALUES( {{?id:int}}, {{?name:string}});
 INSERT INTO orders(item , customerid , price ) 
 VALUES ({{?item:string}},{{?customerid:int}},{{?price:double}});
 )",
+
+    // Annotated selected columns via {{tag:type}} a '?' after type, means to
+    // use std::optional<type> instead of type. Also, annotate the one parameter
+    // price.
     select_orders = R"(
 SELECT  {{orders.id:int}}, {{name:string}},  {{item:string?}}, {{price:double}}
 FROM orders JOIN customers ON customers.id = customerid where price > {{?price:double}}
@@ -59,10 +77,14 @@ int main() {
 
     for (auto &row :
          prepared_select_orders.execute_rows(bind<price>(min_price))) {
+      // Access the fields using `field`. We will get a compiler error if we try
+      // to access a field that is not part of the select statement.
       std::cout << field<orders, id>(row) << " ";
+      // The statement below would cause a compiler error since we did not
+      // select custoers.id.
+      // std::cout << field<customers, id>(row) << " ";
       std::cout << field<price>(row) << " ";
       std::cout << field<name>(row) << " ";
-      // Because we had a '?' in the tag for item, the type is std::optional.
       std::cout << field<item>(row).value() << "\n";
     }
   }
