2d1bc49cd5d9d806db025f34f7d3d0e87edccc43
Your Name <jbandela@gmail.com>
2020-09-20 21:04:35 -0700

    add support to tagged_struct for auto deduced members that access other members of the struct

--- cpp20_tagged_struct/example.cpp
+++ cpp20_tagged_struct/example.cpp
@@ -3,24 +3,24 @@
 
 #include "tagged_struct.h"
 
-
-
-
-
 int main() {
-
-
-  tagged_struct<member<"hello", auto_, []{return 5;}>, member<"world", std::string>,
-  member<"test", int,[](auto& t){return t->*tag<"hello">*2;}>> ts{
-      tag<"hello"> = 1};
+  tagged_struct<member<"hello", auto_, [] { return 5; }>,
+                member<"world", std::string>,
+                member<"test", auto_,
+                       [](auto& t) {
+                         return 2 * get<"hello">(t) + get<"world">(t).size();
+                       }>,
+                member<"last", int, [] { return 0; }>>
+      ts{tag<"hello"> = 1};
 
   using T = decltype(ts);
   T t2{tag<"world"> = std::string("JRB")};
 
   auto hello = tag<"hello">;
-  std::cout << ts->*tag<"hello"> << "\n";
-  std::cout << t2->*tag<"hello"> << "\n";
-  std::cout << t2->*hello << "\n";
-  std::cout << t2->*tag<"world"> << "\n";
-  std::cout << t2->*tag<"test"> << "\n";
+  std::cout << get<"hello">(ts) << "\n";
+  std::cout << get<"world">(ts) << "\n";
+  std::cout << get<"test">(ts) << "\n";
+  std::cout << get<"hello">(t2) << "\n";
+  std::cout << get<"world">(t2) << "\n";
+  std::cout << get<"test">(t2) << "\n";
 }
--- cpp20_tagged_struct/tagged_struct.h
+++ cpp20_tagged_struct/tagged_struct.h
@@ -2,13 +2,16 @@
 #include <algorithm>
 #include <cstdint>
 #include <string_view>
+#include <tuple>
+#include <utility>
+#include <array>
 
 template <std::size_t N>
 struct fixed_string {
   constexpr fixed_string(const char (&foo)[N + 1]) {
     std::copy_n(foo, N + 1, data);
   }
-//  constexpr fixed_string(const fixed_string<N>&) = default;
+  //  constexpr fixed_string(const fixed_string<N>&) = default;
   constexpr fixed_string(std::string_view s) {
     std::copy_n(s.data(), N, data);
   };
@@ -27,6 +30,47 @@ fixed_string(const char (&str)[N]) -> fixed_string<N - 1>;
 template <std::size_t N>
 fixed_string(fixed_string<N>) -> fixed_string<N>;
 
+template <typename Member>
+constexpr auto member_tag() {
+  return Member::fs.sv();
+}
+
+template<typename T>
+struct type_to_type{
+    using type = T;
+};
+
+template <typename... Members>
+struct tagged_struct;
+
+template <typename S, typename M>
+struct chop_to_helper;
+
+template <typename... Members, typename M>
+struct chop_to_helper<tagged_struct<Members...>,M> {
+  template <auto fs>
+  static constexpr std::size_t find_index() {
+    std::array<std::string_view, sizeof...(Members)> ar{
+        member_tag<Members>()...};
+    for (std::size_t i = 0; i < ar.size(); ++i) {
+      if (ar[i] == fs.sv()) return i;
+    }
+  }
+
+  static constexpr std::size_t index = find_index<M::fs>();
+  decltype(std::make_index_sequence<index>()) sequence;
+  using member_tuple = std::tuple<Members...>;
+  template<std::size_t... I>
+  static constexpr auto chop(std::index_sequence<I...>){
+      return type_to_type<tagged_struct<std::tuple_element_t<I,member_tuple>...>>{};
+  }
+
+  using type = typename decltype(chop(sequence))::type;
+};
+
+template<typename S, typename M>
+using chopped = typename chop_to_helper<S,M>::type;
+
 template <typename T>
 constexpr auto default_init = []() { return T{}; };
 
@@ -35,41 +79,45 @@ struct dummy_conversion {};
 template <typename Tag, typename T, auto Init = default_init<T>>
 struct member_impl {
   T value;
-  template<typename Self>
-  member_impl(Self& self, dummy_conversion) 
-      requires requires{{Init()}->std::convertible_to<T>;}
+  template <typename Self>
+  member_impl(Self& self, dummy_conversion) requires requires {
+    { Init() }
+    ->std::convertible_to<T>;
+  }
       : value(Init()) {
   }
 
-  template<typename Self>
-  member_impl(Self& self, dummy_conversion) 
-      requires requires{{Init(self)}->std::convertible_to<T>;}
+      template <typename Self>
+      member_impl(Self& self, dummy_conversion) requires requires {
+        { Init(self) }
+        ->std::convertible_to<T>;
+      }
       : value(Init(self)) {
   }
-  member_impl(T value) : value(std::move(value)) {}
-  member_impl(const member_impl&) = default;
-  member_impl& operator=(const member_impl&) = default;
-  member_impl(member_impl&&) = default;
-  member_impl& operator=(member_impl&&) = default;
-  template <typename Self,typename OtherT, auto OtherInit>
-  member_impl(Self& self, const member_impl<Tag, OtherT, OtherInit>& other)
-      : value(other.value){};
-  template <typename Self, typename OtherT, auto OtherInit>
-  member_impl(Self& self, member_impl<Tag, OtherT, OtherInit>&& other)
-      : value(std::move(other.value)){};
-  template <typename OtherT, auto OtherInit>
-  member_impl& operator=(const member_impl<Tag, OtherT, OtherInit>& other) {
-    value = other.value;
-    return *this;
-  }
-  template <typename OtherT, auto OtherInit>
-  member_impl& operator=(member_impl<Tag, OtherT, OtherInit>&& other) {
-    value = std::move(other.value);
-    return *this;
-  };
+      member_impl(T value) : value(std::move(value)) {}
+      member_impl(const member_impl&) = default;
+      member_impl& operator=(const member_impl&) = default;
+      member_impl(member_impl&&) = default;
+      member_impl& operator=(member_impl&&) = default;
+      template <typename Self, typename OtherT, auto OtherInit>
+      member_impl(Self& self, const member_impl<Tag, OtherT, OtherInit>& other)
+          : value(other.value){};
+      template <typename Self, typename OtherT, auto OtherInit>
+      member_impl(Self& self, member_impl<Tag, OtherT, OtherInit>&& other)
+          : value(std::move(other.value)){};
+      template <typename OtherT, auto OtherInit>
+      member_impl& operator=(const member_impl<Tag, OtherT, OtherInit>& other) {
+        value = other.value;
+        return *this;
+      }
+      template <typename OtherT, auto OtherInit>
+      member_impl& operator=(member_impl<Tag, OtherT, OtherInit>&& other) {
+        value = std::move(other.value);
+        return *this;
+      };
 
-  using tag_type = Tag;
-  using value_type = T;
+      using tag_type = Tag;
+      using value_type = T;
 };
 template <fixed_string fs>
 struct tuple_tag {
@@ -82,19 +130,45 @@ struct tuple_tag {
 
 struct auto_;
 
-template <typename T, auto Init>
+template <typename Self, typename T, auto Init>
 struct t_or_auto {
   using type = T;
 };
 
-template <auto Init>
-struct t_or_auto<auto_, Init> {
+template <typename Self, auto Init>
+requires requires {
+  {Init()};
+}
+struct t_or_auto<Self, auto_, Init> {
   using type = decltype(Init());
 };
 
-template <fixed_string fs, typename T, auto Init = default_init<T>>
-using member =
-    member_impl<tuple_tag<fixed_string<fs.size()>(fs)>, typename t_or_auto<T, Init>::type, Init>;
+template <typename Self, auto Init>
+requires requires {
+  {Init(std::declval<Self&>())};
+}
+struct t_or_auto<Self, auto_, Init> {
+  using type = decltype(Init(std::declval<Self&>()));
+};
+
+template <fixed_string Fs, typename T, auto Init = default_init<T>>
+struct member {
+  using type = T;
+  using fs_type = decltype(Fs);
+  static constexpr fs_type fs = Fs;
+  static constexpr decltype(Init) init = Init;
+};
+
+template <typename Self, typename Member>
+struct member_to_impl {
+  using type = member_impl<
+      tuple_tag<fixed_string<Member::fs.size()>(Member::fs)>,
+      typename t_or_auto<chopped<Self,Member>, typename Member::type, Member::init>::type,
+      Member::init>;
+};
+
+template <typename Self, typename Member>
+using member_to_impl_t = typename member_to_impl<Self, Member>::type;
 
 template <typename Tag, typename T>
 auto make_member_impl(T t) {
@@ -109,18 +183,20 @@ struct parameters : Members... {
 template <typename... Members>
 parameters(Members&&...) -> parameters<std::decay_t<Members>...>;
 
-template <typename... Members>
-struct tagged_struct_base : Members... {
-  template <typename Self, typename... Args>
-  tagged_struct_base(Self& self, parameters<Args...> p) : Members{self,p}... {}
+template <typename Self, typename... Members>
+struct tagged_struct_base : member_to_impl_t<Self, Members>... {
+  template <typename... Args>
+  tagged_struct_base(Self& self, parameters<Args...> p)
+      : member_to_impl_t<Self, Members>{self, p}... {}
 };
 
 template <typename... Members>
-struct tagged_struct : tagged_struct_base<Members...> {
-  using super = tagged_struct_base<Members...>;
+struct tagged_struct
+    : tagged_struct_base<tagged_struct<Members...>, Members...> {
+  using super = tagged_struct_base<tagged_struct, Members...>;
   template <typename... Args>
   tagged_struct(Args&&... args)
-      : super(*this,parameters{std::forward<Args>(args)...}) {}
+      : super(*this, parameters{std::forward<Args>(args)...}) {}
 };
 
 template <typename... Members>
