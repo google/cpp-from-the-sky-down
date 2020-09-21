0a921e57777cec2e873c5235820b513e851f834d
Your Name <jbandela@gmail.com>
2020-09-20 21:28:09 -0700

    Improve example for tagged_struct

--- cpp20_tagged_struct/example.cpp
+++ cpp20_tagged_struct/example.cpp
@@ -5,13 +5,13 @@
 
 int main() {
   tagged_struct<member<"hello", auto_, [] { return 5; }>,
-                member<"world", std::string,[]{}>,
+                member<"world", std::string, [] {}>,
                 member<"test", auto_,
                        [](auto& t) {
                          return 2 * get<"hello">(t) + get<"world">(t).size();
                        }>,
                 member<"last", int, [] { return 0; }>>
-      ts{arg<"hello"> = 1, arg<"world"> = "Universe"};
+      ts{arg<"world"> = "Universe", arg<"hello"> = 1};
 
   using T = decltype(ts);
   T t2{arg<"world"> = "JRB"};
