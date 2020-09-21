bb4ee811972470eca1bbc7f8d145a45df9345566
Your Name <jbandela@gmail.com>
2020-09-20 21:36:13 -0700

    Add example of named arguments

--- cpp20_tagged_struct/example.cpp
+++ cpp20_tagged_struct/example.cpp
@@ -3,6 +3,17 @@
 
 #include "tagged_struct.h"
 
+using test_arguments =
+    tagged_struct<member<"a", int, [] {}>,  // Required argument.
+                  member<"b", auto_, [](auto& t) { return get<"a">(t) + 2; }>,
+                  member<"c", auto_, [](auto& t) { return get<"b">(t) + 2; }>>;
+
+void test(test_arguments args) {
+  std::cout << get<"a">(args) << "\n";
+  std::cout << get<"b">(args) << "\n";
+  std::cout << get<"c">(args) << "\n";
+}
+
 int main() {
   tagged_struct<member<"hello", auto_, [] { return 5; }>,
                 member<"world", std::string, [] {}>,
@@ -22,4 +33,7 @@ int main() {
   std::cout << get<"hello">(t2) << "\n";
   std::cout << get<"world">(t2) << "\n";
   std::cout << get<"test">(t2) << "\n";
+
+  test({arg<"c"> = 1, arg<"a"> = 5});
+  test({arg<"a"> = 1});
 }
