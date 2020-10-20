fae6eb8054517b59c7493bb3af1d362a8dd0264c
Your Name <jbandela@gmail.com>
2020-10-20 08:14:44 -0700

    More variadic examples

--- variadics_examples/tuple_no_variadics_hard_coded_index.cpp
+++ variadics_examples/tuple_no_variadics_hard_coded_index.cpp
@@ -1,17 +1,30 @@
 #include <iostream>
+#include <cstdint>
 
-template<typename T>
+using std::size_t;
+
+template<size_t I, typename T>
 struct Holder{
 	T value;
 };
 
-template<typename T>
-decltype(auto) get(const Holder<T>& h){
+template<typename T, size_t I>
+decltype(auto) get(const Holder<I,T>& h){
 	return h.value;
 }
 
-template<typename T>
-decltype(auto) get(Holder<T>& h){
+template<typename T, size_t I>
+decltype(auto) get(Holder<I,T>& h){
+	return h.value;
+}
+
+template<size_t I,typename T>
+decltype(auto) get(const Holder<I,T>& h){
+	return h.value;
+}
+
+template<size_t I,typename T>
+decltype(auto) get(Holder<I,T>& h){
 	return h.value;
 }
 
@@ -22,16 +35,17 @@ struct tuple;
 
 
 template<typename T0>
-struct tuple<T0>:Holder<T0>{
+struct tuple<T0>:Holder<0,T0>{
 	tuple() = default;
-	tuple(T0 t0):Holder<T0>(std::move(t0)){}
+	tuple(T0 t0):Holder<0,T0>(std::move(t0)){}
 };
 
 template<typename T0,typename T1>
-struct tuple<T0,T1>:Holder<T0>, Holder<T1>{
+struct tuple<T0,T1>:Holder<0,T0>, Holder<1,T1>{
 	tuple() = default;
-	tuple(T0 t0,T1 t1):Holder<T0>(std::move(t0)),Holder<T1>(std::move(t1)){}
+	tuple(T0 t0,T1 t1):Holder<0,T0>(std::move(t0)),Holder<1,T1>(std::move(t1)){}
 };
+
 
 int main(){
 	tuple<int> t{1};
@@ -39,5 +53,7 @@ int main(){
 
 	tuple<int,double> t2{1,3.5};
 	std::cout << get<double>(t2) << "\n";
+	std::cout << get<0>(t2) << "\n";
+	std::cout << get<1>(t2) << "\n";
 
 }
