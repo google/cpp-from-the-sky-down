88477a84e3f037925f392e71dd5a6fb5fde93a2b
John Bandela <jbandela@gmail.com>
2021-04-20 11:35:34 -0700

    Change example for tagged_rusqlite to be similar to the C++ example

--- rust/tagged_rusqlite_example/src/main.rs
+++ rust/tagged_rusqlite_example/src/main.rs
@@ -20,100 +20,106 @@ fn main() -> Result<()> {
     let conn = Connection::open_in_memory()?;
 
     tagged_sql!(
-        CreateTable,
-        "CREATE TABLE person (
+        CreateCustomers,
+        r#"CREATE TABLE customers (
                   id              INTEGER PRIMARY KEY,
-                  name            TEXT NOT NULL,
-                  data            BLOB
-                  );"
+                  name            TEXT NOT NULL
+                  );"#
     );
 
-    CreateTable::prepare(&conn).execute()?;
+    CreateCustomers::prepare(&conn).execute()?;
 
     tagged_sql!(
-        InsertPerson,
-        r#"INSERT INTO
-                person (name, data)
+        CreateOrders,
+        r#"CREATE TABLE orders (
+                  id              INTEGER PRIMARY KEY,
+                  item            TEXT NOT NULL,
+                  customerid      INTEGER NOT NULL,
+                  price           REAL NOT NULL,
+                  discount_code   TEXT
+                  );"#
+    );
+
+
+    CreateOrders::prepare(&conn).execute()?;
+
+    tagged_sql!(
+        InsertCustomer,
+        r#"INSERT INTO  customers (name)
                 VALUES (
-                    ?1 /*:name:String*/,
-                    ?2 /*:data:Option<Vec<u8>>*/
+                    ? /*:name:String*/
                 );
                 "#
     );
 
-    let mut insert = InsertPerson::prepare(&conn);
-    insert.execute_bind(&InsertPersonParams {
-        name: "Steven".to_string(),
-        data: Some(vec![1u8, 2u8]),
-    })?;
-    insert.execute_bind(&InsertPersonParams {
+    let mut insert_customer = InsertCustomer::prepare(&conn);
+    insert_customer.execute_bind(&InsertCustomerParams {
         name: "John".to_string(),
-        data: None,
-    })?;
-    insert.execute_bind(&InsertPersonParams {
-        name: "Bill".to_string(),
-        data: None,
     })?;
 
     tagged_sql!(
-        SelectPerson,
-        r#"SELECT
-                            id   /*:i64*/,
-                            name /*:String*/,
-                            data /*:Option<Vec<u8>>*/
-                            FROM person;
-                            "#
+        InsertOrder,
+        r#"
+        INSERT into orders(item, customerid, price, discount_code) 
+        VALUES(
+                ? /*:item:String*/,
+                ? /*:customerid:i64*/,
+                ? /*:price:f64*/,
+                ? /*:discount_code:Option<String>*/
+        )
+        "#
     );
-
-    let mut stmt = SelectPerson::prepare(&conn);
-    let person_iterator = stmt.query()?;
-
-    for person in person_iterator {
-        println!("Found person {:?}", &person.unwrap());
-    }
 
     tagged_sql!(
-        SearchByName,
-        r#"SELECT
-                            id   /*:i64*/,
-                            name /*:String*/,
-                            data /*:Option<Vec<u8>>*/
-                            FROM person
-                            WHERE name = ? /*:name:String*/;
-                            "#
+        FindCustomer,
+        r#"
+        SELECT id /*:i64*/ from customers
+        where name = ? /*:name:String*/
+        "#
     );
-    let search_params = SearchByNameParams {
+
+    let customerid: i64 = FindCustomer::prepare(&conn).query_row_bind(&FindCustomerParams {
         name: "John".into(),
-    };
-    for person in SearchByName::prepare(&conn).query_bind(&search_params)? {
-        println!(
-            "Found person with name {}  {:?}",
-            &search_params.name,
-            &person.unwrap()
-        );
-    }
+    })?.id;
 
-    let mut stmt = SelectPerson::prepare(&conn);
-    println!("Found single person {:?} ", stmt.query_row()?);
+    let mut insert_order = InsertOrder::prepare(&conn);
+    insert_order.execute_bind(&InsertOrderParams {
+        item: "Phone".into(),
+        price: 1444.44,
+        customerid,
+        discount_code: None
+    })?;
+    insert_order.execute_bind(&InsertOrderParams {
+        item: "Laptop".into(),
+        price: 1300.44,
+        customerid,
+        discount_code: None
+    })?;
+    insert_order.execute_bind(&InsertOrderParams {
+        item: "Laptop".into(),
+        price: 1300.44,
+        customerid,
+        discount_code: Some("BIGSALE".to_owned())
+    })?;
 
-    let mut stmt = SearchByName::prepare(&conn);
-    let search_params = SearchByNameParams {
-        name: "Bill".into(),
-    };
-    println!(
-        "Found single person with name {}, {:?} ",
-        &search_params.name,
-        stmt.query_row_bind(&search_params)?
+    tagged_sql!(
+        FindOrders,
+        r#"
+        SELECT
+            orders.id as oid /*:i64*/,
+            name /*:String*/,
+            item /*:String*/,
+            price /*:f64*/,
+            discount_code /*:Option<String>*/
+            FROM orders JOIN customers on customers.id = customerid
+            WHERE price > ?/*:min_price:f64*/
+        "#
     );
 
+    let mut find_orders = FindOrders::prepare(&conn);
+    for orders in find_orders.query_bind(&FindOrdersParams { min_price: 100.0 })? {
+        println!("{:?}", &orders);
+    }
     Ok(())
 }
 
-/* Output
-Found person SelectPersonRow { id: 1, name: "Steven", data: Some([1, 2]) }
-Found person SelectPersonRow { id: 2, name: "John", data: None }
-Found person SelectPersonRow { id: 3, name: "Bill", data: None }
-Found person with name John  SearchByNameRow { id: 2, name: "John", data: None }
-Found single person SelectPersonRow { id: 1, name: "Steven", data: Some([1, 2]) }
-Found single person with name Bill, SearchByNameRow { id: 3, name: "Bill", data: None }
- */
